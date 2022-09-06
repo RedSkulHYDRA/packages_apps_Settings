490b6c0857f357a5184c8471bd044e901f3ab62f
Jason Edson <jaysonedson@gmail.com>
2022-09-06 11:45:29 -0700

    Settings: Add Security Hub from Pixel devices
    Obviously it needs the SecurityHubPrebuilt.apk installed
    on your system for it to work correctly.
    
    Change-Id: I3843f6134743db9802bc5c84bc4f609928549081
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -181,6 +181,8 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.yasp.settings.YaapSettings;
 import ink.kscope.settings.wifi.tether.WifiTetherClientManager;
 
+import com.google.android.settings.security.SecurityHubDashboard;
+
 public class SettingsGateway {
 
     /**
@@ -349,7 +351,8 @@ public class SettingsGateway {
             RunningServices.class.getName(),
             WirelessDebuggingFragment.class.getName(),
             YaapSettings.class.getName(),
-            WifiTetherClientManager.class.getName()
+            WifiTetherClientManager.class.getName(),
+            SecurityHubDashboard.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
